71357b989ffe9c52e25965829fe6d6750b51471b
稲垣秦一郎 <inagakishinichiro@ingk-MacBook-Pro.local>
2019-09-01 15:39:47 +0900

    add main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from character import Character
+from tqdm import tqdm
+import re
+import csv
+
+with open("/Users/inagakishinichiro/Desktop/py3works/FONT/kanji.txt") as f:
+    characters = f.readlines()[0]
+
+font_list = ["/System/Library/Fonts/ヒラギノ明朝 ProN.ttc",
+             "/System/Library/Assets/com_apple_MobileAsset_Font5/458cb75c37483d7bcdfd68445b7246c76ecb29a6.asset"
+             "/AssetData/Osaka.ttf",
+             "/System/Library/Assets/com_apple_MobileAsset_Font5/1765bae07bcefebc499ababa7ecdef963e4f151e.asset"
+             "/AssetData/Klee.ttc"]
+
+if __name__ == '__main__':
+    for font in font_list:
+        with open("output/{}_log.csv".format(re.findall(r'(.*/)?(.+)(\.[a-z]{3})', font)[0][-2]), 'a') as f:
+            writer = csv.writer(f, lineterminator='\n')
+            writer.writerow(("character", "G", "M"))
+            for char in tqdm(characters):
+                c = Character(char=char, font=font, size=(800, 800))
+                c.main()
+                writer.writerow((c, c.G, c.M))
