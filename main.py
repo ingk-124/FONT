550aaf51d679db7905bb686631f9fd81b57a2b66
inagakishinichiro <sinina124aikido@gmail.com>
2019-09-03 01:20:54 +0900

    correction

--- main.py
+++ main.py
@@ -16,10 +16,10 @@ if __name__ == '__main__':
     for font in font_list:
         with open("output/{}_2nd_log.csv".format(re.findall(r'(.*/)?(.+)(\.[a-z]{3})', font)[0][-2]), 'a') as f:
             writer = csv.writer(f, lineterminator='\n')
-            writer.writerow(("","character", "G_x", "G_y", "M", "Euclid"))
+            writer.writerow(("character", "G_x", "G_y", "M", "Euclid"))
             with tqdm(characters) as pbar:
                 for i, char in enumerate(pbar):
                     pbar.set_description("[{}:{}]" .format(i, char))
                     c = Character(char=char, font=font, size=(800, 800))
                     c.main()
-                    writer.writerow((i, char, c.G[0], c.G[1], c.M, c.euclid))
+                    writer.writerow((char, c.G[0], c.G[1], c.M, c.euclid))
