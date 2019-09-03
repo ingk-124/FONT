cfb7ede676543b2e0311e4acd60fb148ae69deaf
inagakishinichiro <sinina124aikido@gmail.com>
2019-09-03 13:37:03 +0900

    correction

--- main.py
+++ main.py
@@ -3,23 +3,22 @@ from tqdm import tqdm
 import re
 import csv
 
-with open("joyo_kanji.txt") as f:
+with open("kanji.txt") as f:
     characters = f.readlines()[0]
 
 font_list = ["/System/Library/Fonts/ヒラギノ明朝 ProN.ttc",
-             "/System/Library/Assets/com_apple_MobileAsset_Font5/458cb75c37483d7bcdfd68445b7246c76ecb29a6.asset"
-             "/AssetData/Osaka.ttf",
+             "/System/Library/Fonts/ヒラギノ角ゴシック W3.ttc",
              "/System/Library/Assets/com_apple_MobileAsset_Font5/1765bae07bcefebc499ababa7ecdef963e4f151e.asset"
              "/AssetData/Klee.ttc"]
 
 if __name__ == '__main__':
-    for font in font_list:
-        with open("output/{}.csv".format(re.findall(r'(.*/)?(.+)(\.[a-z]{3})', font)[0][-2]), 'a') as f:
+    for font in font_list[1:2]:
+        with open("output/{}.csv".format(re.findall(r'(.*/)?(.+)(\.[a-z]{3})', font)[0][-2]), 'w') as f:
             writer = csv.writer(f, lineterminator='\n')
             writer.writerow(("character", "G_x", "G_y", "M", "Euclid"))
             with tqdm(characters) as pbar:
                 for i, char in enumerate(pbar):
                     pbar.set_description("[{}:{}]" .format(i, char))
-                    c = Character(char=char, font=font, size=(800, 800))
+                    c = Character(char=char, font=font, size=(500, 500))
                     c.main()
                     writer.writerow((char, c.G[0], c.G[1], c.M, c.euclid))
